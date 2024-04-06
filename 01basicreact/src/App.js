6391c9ef537f4b6455038b9251a290ae66e42ba7
ezbhai <varundev@gamil.com>
2024-04-06 11:00:05 +0530

    first project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Codingbhai from "./codingbhai";
+
+function App() {
+  return (
+    <>
+    <Codingbhai/>
+    <h1>react aur coding bhai </h1>
+    </>
+    
+  );
+}
+
+export default App;
+
---
+++ 01basicreact/src/Codingbhai.js
@@ -0,0 +1,7 @@
+function Codingbhai(){
+    return(
+        <h1>coding bhai is ready</h1>
+    )
+}
+
+export default Codingbhai
