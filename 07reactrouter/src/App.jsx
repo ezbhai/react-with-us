6e25eb39905fb8ac4d1d1158f45c61580ade3533
ezbhai <varundev@gamil.com>
2024-04-12 10:50:16 +0530

    react router mini project

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './component/Header/Header'
+import Home from './component/Home/Home'
+import Footer from './component/Footer/Footer'
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
